19ae8fedc5760f2c3bd4fff3e8f4d6ec4a66721a
Michael Hart <michael.hart1994@gmail.com>
2017-03-09 12:19:24 +0000

    Added disclaimer on app start-up

--- ManDown/mobile/src/main/java/net/mandown/MainActivity.java
+++ ManDown/mobile/src/main/java/net/mandown/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.pm.ActivityInfo;
 import android.media.Image;
 import android.os.Handler;
 import android.os.IBinder;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -20,6 +21,13 @@ import net.mandown.sensors.SensorService;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    private final String mDisclaimerText =
+            "This app is distributed for the collection of accelerometer, gyroscope, and " +
+            "magnetometer data over time. The use of the app's games will collect information " +
+            "and store it online. The app does not accept responsibility for inaccurate readings " +
+            "or results for intoxication levels.\n\nIf you wish to opt out, please uninstall " +
+            "the application.";
 
     // Set up a new handler to update the home textview with number of DB entries every 100ms
     private final Handler mDbUpdateHandler = new Handler();
@@ -58,6 +66,13 @@ public class MainActivity extends AppCompatActivity {
         // Post event to handler to begin DB updates
         mDbUpdateHandler.postDelayed(mUpdateDBTxt, 100);
 
+        // Create a disclaimer window using AlertDialog
+        AlertDialog dialog = (new AlertDialog.Builder(this))
+                .setTitle("ManDown Disclaimer")
+                .setMessage(mDisclaimerText)
+                .setPositiveButton("I understand", null)
+                .create();
+        dialog.show();
     }
 
 }
