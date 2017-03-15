37146eb69646a0953285fd9e9ad2a02ca9f82cbe
Michael Hart <michael.hart1994@gmail.com>
2017-03-15 01:13:30 +0000

    Updated history view with simple string list

--- ManDown/mobile/src/main/java/net/mandown/history/HistoryActivity.java
+++ ManDown/mobile/src/main/java/net/mandown/history/HistoryActivity.java
@@ -1,63 +1,40 @@
 package net.mandown.history;
 
 import android.Manifest;
-import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.DisplayMetrics;
-import android.util.Log;
-import android.view.Display;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.SurfaceHolder;
-import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
-import android.widget.LinearLayout;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
-import net.mandown.MainActivity;
 import net.mandown.R;
-import net.mandown.db.DBService;
-import net.mandown.games.GameMenuActivity;
-import android.view.SurfaceView;
-import android.widget.TextView;
 
-
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.ValueEventListener;
-
-import java.util.Map;
-
-import static java.security.AccessController.getContext;
-
+import java.util.ArrayList;
 
 public class HistoryActivity extends AppCompatActivity  {
 
+    // UI Controls
     private ImageButton btnBeerGlass;
+    private ListView lvHistory;
 
-//    private Thread beerThread = null;
+    // List adapter for lvHistory
+    ListAdapter mHistoryAdapter;
 
     private Toolbar toolbar;
     private static final int REQUEST_PHONE_CALL = 1;
-
-
 
     private final String mDisclaimerText =
             "This app is distributed for the collection of accelerometer, gyroscope, and " +
@@ -66,8 +43,6 @@ public class HistoryActivity extends AppCompatActivity  {
                     "or results for intoxication levels.\n\nIf you wish to opt out, please uninstall " +
                     "the application.";
 
-    //  private DisplayMetrics dm;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -75,16 +50,29 @@ public class HistoryActivity extends AppCompatActivity  {
         //setting the orientation to landscape
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
-
-
-
+        // Set up toolbar at the top
         toolbar = (Toolbar) findViewById(R.id.tool_bar_history);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("History");
 
+        // Set up data for History View
+        ArrayList<String> myNewList = new ArrayList<>();
+        myNewList.add("Testing string");
+        myNewList.add("Testing string");
+        myNewList.add("Testing string");
+        myNewList.add("Testing string");
+        myNewList.add("Testing string");
+        myNewList.add("Testing string");
+        myNewList.add("Testing string");
+        myNewList.add("Testing string");
+        mHistoryAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, myNewList);
 
+        // Get reference to UI Controls
         btnBeerGlass = (ImageButton) findViewById(R.id.BeerGlass);
+        lvHistory = (ListView) findViewById(R.id.lvHistoryView);
+        lvHistory.setAdapter(mHistoryAdapter);
 
+        // Set to fully drunk!
         update_drunk_level(2);
 
     }
@@ -167,22 +155,11 @@ public class HistoryActivity extends AppCompatActivity  {
                         .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialog, int which) {
                                 // continue with action
-
                                 Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + "I'm Drunk"));
-
                                 if (ContextCompat.checkSelfPermission(HistoryActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
                                     ActivityCompat.requestPermissions(HistoryActivity.this, new String[]{Manifest.permission.CALL_PHONE},REQUEST_PHONE_CALL);
                                 }
-                                else
-                                {
-                                    startActivity(intent);
-                                }
-
-                            }
-                        })
-                        .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int which) {
-                                // do nothing
+                                startActivity(intent);
                             }
                         })
                         .setIcon(android.R.drawable.ic_dialog_alert)
